ba3ba3746fa6dc08ab3b8a0c067a65453b073d98
lx <lx@csair.com>
2020-04-06 16:09:52 +0800

    project init

---
+++ src/main/java/com/csair/eclipse/Eclipse01Application.java
@@ -0,0 +1,13 @@
+package com.csair.eclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eclipse01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eclipse01Application.class, args);
+	}
+
+}
